a72ecc0a06270b63cc0084327fb01322815c3b67
Yonglin Li <yonglin.li@gmail.com>
2020-05-22 01:12:38 -0400

    get-launches

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ApolloClient from "apollo-boost";
+// import { ApolloClient } from 'apollo-client';
+import { ApolloProvider } from "react-apollo";
+
+import Launches from './components/Launches';
+
+import "./App.css";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>SpaceX</h1>
+      </div>
+      <Launches />
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/components/LaunchItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const LaunchItem = (props) => {
+  console.log(props);
+
+  return (
+    <div className="card card-body mb-3">
+        <div className="row">
+            <div className="col-md-9">
+                <h4>Mission: {props.mission_name} </h4>
+                <p>Date: {props.launch_date_local} </p>
+            </div>
+            <div className="col-md-3">
+                <button className={`btn btn-secondary`}>Deatils {props.flight_number}</button>
+            </div>
+            
+        </div>
+    </div>
+  );
+};
+
+export default LaunchItem;
